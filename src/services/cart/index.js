70d2485ab5e1dc7d72acecff4eb87df7563fc784
harshpa324 <harshpa324@gmail.com>
2023-11-14 14:28:27 +0530

    netlify

--- src/services/cart/index.js
+++ src/services/cart/index.js
@@ -21,7 +21,7 @@ export const addToCart = async (formData) => {
 
 export const getAllCartItems = async (id) => {
   try {
-    const res = await fetch(`https://shivajewellers.netify.app/api/cart/all-cart-items?id=${id}`, {
+    const res = await fetch(`https://shivajewellers.netlify.app/api/cart/all-cart-items?id=${id}`, {
       method: "GET",
       headers: {
         Authorization: `Bearer ${Cookies.get("token")}`,
