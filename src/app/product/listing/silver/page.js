00bd70d926a38f3c97b330bd78556db6ef154df3
Harsh Pal <harshpa324@gmail.com>
2023-11-20 20:48:47 +0530

    finishing touch

--- src/app/product/listing/silver/page.js
+++ src/app/product/listing/silver/page.js
@@ -1,8 +1,8 @@
 import CommonListing from "@/components/CommonListing";
-import { productByCategory } from "@/services/product";
+import { productBysubCategory } from "@/services/product";
 
 export default async function MenAllProducts() {
-  const getAllProducts = await productByCategory("necklace");
+  const getAllProducts = await productsubByCategory("necklace");
 
   return <CommonListing data={getAllProducts && getAllProducts.data} />;
 }
